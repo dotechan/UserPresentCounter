37fbd7df1faee85b66bb0191db84c63e02577c00
ito-hi <this.is.itoooo.0120@outlook.jp>
2019-12-10 23:31:42 +0900

    Implement boot receiver.

--- app/src/main/java/com/example/user/present/counter/BootBroadcastReceiver.java
+++ app/src/main/java/com/example/user/present/counter/BootBroadcastReceiver.java
@@ -1,6 +1,37 @@
 package com.example.user.present.counter;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: ");
+        boolean bootCompleted;
+        String action = intent.getAction();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            bootCompleted = Intent.ACTION_LOCKED_BOOT_COMPLETED.equals(action);
+        } else {
+            bootCompleted = Intent.ACTION_BOOT_COMPLETED.equals(action);
+        }
+        if (!bootCompleted) {
+            return;
+        }
+
+        Intent bootServiceIntent = new Intent(context.getApplicationContext(), BootService.class);
+        // Android8.0よりバックグラウンドからバックグラウンドサービスを起動することはできない
+        // startForegroundService()を利用することで新しいサービスを実行することができる
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(bootServiceIntent);
+        } else {
+            context.startService(bootServiceIntent);
+        }
+
+    }
 }
--- app/src/main/java/com/example/user/present/counter/BootService.java
+++ app/src/main/java/com/example/user/present/counter/BootService.java
@@ -1,6 +1,45 @@
 package com.example.user.present.counter;
 
 import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
 
 public class BootService extends Service {
+
+    private static final String TAG = "BootService";
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate: ");
+        super.onCreate();
+    }
+
+    @Override
+    public ComponentName startForegroundService(Intent service) {
+        Log.d(TAG, "startForegroundService: ");
+        return super.startForegroundService(service);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
 }
