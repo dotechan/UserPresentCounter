4cb206ffeae9cdb24dc1e8dee271503b7405cf23
ito-hi <this.is.itoooo.0120@outlook.jp>
2020-01-01 17:36:43 +0900

    Implement MainActivity.

--- app/src/main/java/com/example/user/present/counter/MainActivity.java
+++ app/src/main/java/com/example/user/present/counter/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.user.present.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: ");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        final int originalUnlockCount =
+                sharedPreferences.getInt(getString(R.string.saved_unlock_count_key),
+                        getResources().getInteger(R.integer.initial_unlock_count));
+
+        TextView countTextView = findViewById(R.id.user_present_count);
+        countTextView.setText("count : " + originalUnlockCount);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
     }
 }
