db4535a0c1a5dfd3b3bc2eb25c0af0f9bae1fbd0
ito-hi <this.is.itoooo.0120@outlook.jp>
2020-01-01 18:08:02 +0900

    Add reset button.

--- app/src/main/java/com/example/user/present/counter/MainActivity.java
+++ app/src/main/java/com/example/user/present/counter/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button resetButton = findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: SharedPreferencesへのアクセスを行うクラスに切り出した方がいい
+                // TODO: SharedPreferencesの更新後に画面表示の更新処理も行う必要あり
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(
+                        getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt(getString(R.string.saved_unlock_count_key),
+                        getResources().getInteger(R.integer.initial_unlock_count));
+                editor.commit();
+            }
+        });
     }
 
     @Override
