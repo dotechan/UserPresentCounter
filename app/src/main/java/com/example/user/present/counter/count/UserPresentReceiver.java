bb099f6af69e286160a069e56b0b6350b9a9c3a3
ito-hi <this.is.itoooo.0120@outlook.jp>
2020-01-06 00:12:07 +0900

    Changed UserPresentService from foreground service to background service.

--- app/src/main/java/com/example/user/present/counter/count/UserPresentReceiver.java
+++ app/src/main/java/com/example/user/present/counter/count/UserPresentReceiver.java
@@ -3,7 +3,6 @@ package com.example.user.present.counter.count;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -17,15 +16,8 @@ public class UserPresentReceiver extends BroadcastReceiver {
         Log.d(TAG, "onReceive: " + intent.getAction());
 
         Intent serviceIntent = new Intent(context.getApplicationContext(), UserPresentService.class);
-        // Android8.0よりバックグラウンドからバックグラウンドサービスを起動することはできない
-        // startForegroundService()を利用することで新しいサービスを実行することができる
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            context.startForegroundService(serviceIntent);
-            // TODO: フォアグラウンドサービスからバックグラウンドサービスを起動することはできたのでUserPresentServiceはバックグラウンドサービスに修正する
-            context.startService(serviceIntent);
-        } else {
-            context.startService(serviceIntent);
-        }
+        // フォアグラウンドサービスを起動中であればバックグラウンドサービスを起動することができる
+        context.startService(serviceIntent);
     }
 
     @NonNull
--- app/src/main/java/com/example/user/present/counter/count/UserPresentService.java
+++ app/src/main/java/com/example/user/present/counter/count/UserPresentService.java
@@ -50,9 +50,6 @@ public class UserPresentService extends Service {
         editor.putInt(getString(R.string.saved_unlock_count_key), currentUnlockCount);
         editor.commit();
 
-        createNotificationChannel();
-        startForeground(1, createNotification());
-
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -77,33 +74,5 @@ public class UserPresentService extends Service {
     @Override
     protected void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
         super.dump(fd, writer, args);
-    }
-
-    private Notification createNotification() {
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        Notification notification = new NotificationCompat.Builder(this, "temp_id")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("user present service title")
-                .setContentText("user present service content text")
-                .setPriority(NotificationCompat.PRIORITY_LOW)
-                .build();
-
-        return notification;
-    }
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            NotificationChannel channel =
-                    new NotificationChannel("temp_id", name, NotificationManager.IMPORTANCE_LOW);
-            channel.setDescription(description);
-
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 }
