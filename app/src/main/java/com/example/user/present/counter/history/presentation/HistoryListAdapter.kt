98dbf2a3877c4cd52e5a24b3e68d0bb24c04f00f
dotechan <this.is.itoooo.0120@gmail.com>
2020-09-22 22:47:31 +0900

    リスト要素の生成に関する知識がAdapterに散財していたのを整理した

--- app/src/main/java/com/example/user/present/counter/history/presentation/HistoryListAdapter.kt
+++ app/src/main/java/com/example/user/present/counter/history/presentation/HistoryListAdapter.kt
@@ -27,7 +27,8 @@ class HistoryListAdapter internal constructor(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder =
-            HistoryViewHolder(inflater.inflate(getLayoutRes(viewType), parent, false))
+            HistoryViewHolder(inflater
+                    .inflate(HistoryListItem.getLayoutRes(viewType), parent, false))
 
     override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
         val item = historyRecyclerViewList[position]
@@ -48,32 +49,13 @@ class HistoryListAdapter internal constructor(
 
     override fun getItemCount() = historyRecyclerViewList.size
 
-    override fun getItemViewType(position: Int): Int {
-        return when (historyRecyclerViewList[position]) {
-            is HistoryListItem.HeaderItem -> VIEW_TYPE_HEADER
-            is HistoryListItem.HistoryItem -> VIEW_TYPE_HISTORY
-            is HistoryListItem.BorderItem -> VIEW_TYPE_BORDER
-        }
-    }
-
-    private fun getLayoutRes(viewType: Int) =
-            when (viewType) {
-                VIEW_TYPE_HEADER -> R.layout.item_header
-                VIEW_TYPE_HISTORY -> R.layout.item_history
-                VIEW_TYPE_BORDER -> R.layout.item_border
-                else -> throw IllegalArgumentException("Unknown viewType $viewType")
-            }
+    override fun getItemViewType(position: Int): Int =
+         historyRecyclerViewList[position].itemViewType
 
     internal fun setHistoryList(historyList: List<History>) {
         // TODO: 毎回リストを再作成していたらパフォーマンスが悪いので、リスト生成済みであれば追加するように修正する
         historyRecyclerViewList = HistoryRecyclerViewList(historyList).list
         // TODO: パフォーマンスチューニングする。必要なデータの更新だけ通知すればよい
         notifyDataSetChanged()
-    }
-
-    companion object {
-        private const val VIEW_TYPE_HEADER = 1
-        private const val VIEW_TYPE_HISTORY = 2
-        private const val VIEW_TYPE_BORDER = 3
     }
 }
--- app/src/main/java/com/example/user/present/counter/history/presentation/HistoryListItem.kt
+++ app/src/main/java/com/example/user/present/counter/history/presentation/HistoryListItem.kt
@@ -1,9 +1,25 @@
 package com.example.user.present.counter.history.presentation
 
+import com.example.user.present.counter.R
 import com.example.user.present.counter.history.domain.History
 
-sealed class HistoryListItem {
-    class HeaderItem(val label: String) : HistoryListItem()
-    class HistoryItem(val history: History) : HistoryListItem()
-    object BorderItem : HistoryListItem()
+sealed class HistoryListItem(val itemViewType: Int) {
+    class HeaderItem(val label: String) : HistoryListItem(VIEW_TYPE_HEADER)
+    class HistoryItem(val history: History) : HistoryListItem(VIEW_TYPE_HISTORY)
+    object BorderItem : HistoryListItem(VIEW_TYPE_BORDER)
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 1
+        private const val VIEW_TYPE_HISTORY = 2
+        private const val VIEW_TYPE_BORDER = 3
+
+        fun getLayoutRes(viewType: Int): Int {
+            return when (viewType) {
+                VIEW_TYPE_HEADER -> R.layout.item_header
+                VIEW_TYPE_HISTORY -> R.layout.item_history
+                VIEW_TYPE_BORDER -> R.layout.item_border
+                else -> throw IllegalArgumentException("Unknown viewType $viewType")
+            }
+        }
+    }
 }
